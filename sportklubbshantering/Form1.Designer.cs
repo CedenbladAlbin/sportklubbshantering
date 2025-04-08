72a7f72ef7a71f1c00b22d6f8274a560f70c17ce
niklasgullstrand <105774087+niklasgullstrand@users.noreply.github.com>
2025-04-08 12:51:31 +0200

    Det bästa någonsin!

--- sportklubbshantering/Form1.Designer.cs
+++ sportklubbshantering/Form1.Designer.cs
@@ -20,27 +20,76 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            SuspendLayout();
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(832, 450);
-            Name = "Form1";
-            Text = "Form1";
-            Load += Form1_Load;
-            ResumeLayout(false);
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			button1 = new Button();
+			button2 = new Button();
+			button3 = new Button();
+			textBox1 = new TextBox();
+			SuspendLayout();
+			// 
+			// button1
+			// 
+			button1.Location = new Point(106, 381);
+			button1.Name = "button1";
+			button1.Size = new Size(75, 23);
+			button1.TabIndex = 0;
+			button1.Text = "button1";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click_1;
+			// 
+			// button2
+			// 
+			button2.Location = new Point(703, 391);
+			button2.Name = "button2";
+			button2.Size = new Size(75, 23);
+			button2.TabIndex = 1;
+			button2.Text = "button2";
+			button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			button3.Location = new Point(489, 151);
+			button3.Name = "button3";
+			button3.Size = new Size(75, 23);
+			button3.TabIndex = 2;
+			button3.Text = "button3";
+			button3.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			textBox1.Location = new Point(242, 151);
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(230, 23);
+			textBox1.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(832, 450);
+			Controls.Add(textBox1);
+			Controls.Add(button3);
+			Controls.Add(button2);
+			Controls.Add(button1);
+			Name = "Form1";
+			Text = "Form1";
+			Load += Form1_Load;
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
-    }
+		#endregion
+
+		private Button button1;
+		private Button button2;
+		private Button button3;
+		private TextBox textBox1;
+	}
 }
--- sportklubbshantering/Form1.cs
+++ sportklubbshantering/Form1.cs
@@ -1,20 +1,25 @@
 namespace sportklubbshantering
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+		private void Form1_Load(object sender, EventArgs e)
+		{
 
-        }
+		}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
+		private void button1_Click(object sender, EventArgs e)
+		{
 
-        }
-    }
+		}
+
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+
+		}
+	}
 }
