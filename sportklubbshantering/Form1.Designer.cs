56322214b1cded608c238fda649181f373218dda
niklasgullstrand <105774087+niklasgullstrand@users.noreply.github.com>
2025-04-08 11:40:00 +0200

    En knapp till

--- sportklubbshantering/Form1.Designer.cs
+++ sportklubbshantering/Form1.Designer.cs
@@ -20,41 +20,53 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            button1 = new Button();
-            SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(328, 173);
-            button1.Name = "button1";
-            button1.Size = new Size(123, 23);
-            button1.TabIndex = 0;
-            button1.Text = "Tryck på mig";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(832, 450);
-            Controls.Add(button1);
-            Name = "Form1";
-            Text = "Form1";
-            Load += Form1_Load;
-            ResumeLayout(false);
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			button1 = new Button();
+			button2 = new Button();
+			SuspendLayout();
+			// 
+			// button1
+			// 
+			button1.Location = new Point(131, 237);
+			button1.Name = "button1";
+			button1.Size = new Size(123, 23);
+			button1.TabIndex = 0;
+			button1.Text = "Tryck på mig";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
+			// 
+			// button2
+			// 
+			button2.Location = new Point(378, 237);
+			button2.Name = "button2";
+			button2.Size = new Size(123, 23);
+			button2.TabIndex = 1;
+			button2.Text = "Tryck på mig";
+			button2.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(832, 450);
+			Controls.Add(button2);
+			Controls.Add(button1);
+			Name = "Form1";
+			Text = "Form1";
+			Load += Form1_Load;
+			ResumeLayout(false);
+		}
 
-        #endregion
+		#endregion
 
-        private Button button1;
-    }
+		private Button button1;
+		private Button button2;
+	}
 }
